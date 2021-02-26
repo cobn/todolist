227a8073952939b3d3e91fd4caf0eef69a4d201c
cobn <muhammet2_1996@hotmail.com>
2021-02-26 15:46:34 +0300

    Update Item.php

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,20 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Scout\Searchable;
 class Item extends Model
 {
 
-    use HasFactory,Searchable;
+    use HasFactory;
     protected $fillable = [
         'uid','content','state'
     ];
-    public function toSearchableArray()
-    {
-        $array = $this->toArray();
 
-        // Customize the data array...
-
-        return $array;
-    }
 }
